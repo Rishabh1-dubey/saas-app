84123cc9f39d4e0922a9f4a90fd98d2c2b703a19
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-05-26 14:25:26 +0530

    adding companion page

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,15 @@
+import CompanionFrom from '@/components/CompanionFrom'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article className='w-2/3  gap-4 flex  flex-col' >
+        <h1 className='mb-2'> Companion Buiulder</h1>
+        <CompanionFrom/>
+      </article>
+
+    </main>
   )
 }
 
